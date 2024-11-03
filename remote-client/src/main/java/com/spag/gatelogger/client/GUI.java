d78786079892053673381e280e1ab963eef3942d
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2024-11-03 14:49:11 +1300

    Added Gate List GUI elements

--- remote-client/src/main/java/com/spag/gatelogger/client/GUI.java
+++ remote-client/src/main/java/com/spag/gatelogger/client/GUI.java
@@ -1,7 +1,8 @@
 package com.spag.gatelogger.client;
 
 import com.spag.gatelogger.client.data.Gate;
-
+import java.awt.BorderLayout;
+import java.awt.Font;
 import javax.swing.*;
 
 public class GUI extends JFrame {
@@ -13,7 +14,20 @@ public class GUI extends JFrame {
     setContentPane(new Box(BoxLayout.X_AXIS));
     JList<Gate> knownGatesList = new JList<>(this.knownGates);
     knownGatesList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-    add(knownGatesList);
+    knownGatesList.setAlignmentY(TOP_ALIGNMENT);
+    {
+      JPanel gateList = new JPanel(new BorderLayout());
+      JLabel gatesLabel = new JLabel("Gates", JLabel.CENTER);
+      gatesLabel.setFont(
+          new Font(gatesLabel.getFont().getName(), gatesLabel.getFont().getStyle(), 20));
+      gateList.add(gatesLabel, BorderLayout.NORTH);
+      gateList.getComponent(0);
+      gateList.add(knownGatesList, BorderLayout.CENTER);
+      add(gateList);
+    }
+    knownGates.addElement(new Gate("a", "Chulak"));
+    knownGates.addElement(new Gate("b", "Gallifrey"));
+    knownGates.addElement(new Gate("c", "Kronos"));
 
     add(new JButton("2"));
     add(new JButton("three"));
