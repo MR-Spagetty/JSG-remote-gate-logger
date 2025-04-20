545395ee3f93a38946ce1d5122d5e113dca7e551
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-04-20 23:58:59 +1200

    added comments to template config file

--- gateClient/config.lua
+++ gateClient/config.lua
@@ -1,5 +1,11 @@
 local data = {
+  -- the address of the control server
   address = "0.0.0.0",
+  -- the port of the control server
   port = 123456,
-  name = "gateName"
+  -- the name of this gate
+  name = "gateName",
+  -- whether to reboot on failure to connect to server or not
+  rebootOnFailure=true
 }
+return data
