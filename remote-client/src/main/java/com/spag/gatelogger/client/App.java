e56e8b816b2621ce75bcffcf0d5251cd2129c72e
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-04-20 23:56:53 +1200

    added useage guides to remote client

--- remote-client/src/main/java/com/spag/gatelogger/client/App.java
+++ remote-client/src/main/java/com/spag/gatelogger/client/App.java
@@ -2,6 +2,14 @@ package com.spag.gatelogger.client;
 
 public class App {
   public static void main(String[] args) {
+    if (args.length < 2) {
+      System.out.println("Usage: remote-client <serverAddress> <serverPort>");
+      System.exit(1);
+    } else if (args.length > 2) {
+      System.out.println(
+          "$d arguments given, only 2 expected\nextra arguments given will be ignored"
+              .formatted(args.length));
+    }
     GUI.GUI.setVisible(true);
     System.out.println(Server.connect(args[0], Integer.parseInt(args[1])));
   }
